18fcb31479c2ab135d9d6da0f0b06d3768fce8b3
Radik Friedrichs <radik.fr@proton.me>
2025-07-04 15:45:05 +0200

    Refactor logging in Cluster and add ClusterLogger for improved log management

--- cluster/src/main/kotlin/dev/easycloud/Cluster.kt
+++ cluster/src/main/kotlin/dev/easycloud/Cluster.kt
@@ -1,34 +1,39 @@
 package dev.easycloud
 
-import com.akuleshov7.ktoml.file.TomlFileReader
 import com.akuleshov7.ktoml.file.TomlFileWriter
+import dev.easycloud.terminal.JLineTerminal
+import dev.easycloud.terminal.command.CommandService
+import dev.easycloud.terminal.logger.ClusterLogger
 import dev.easycloud.toml.ClusterToml
-import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.serializer
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.exists
-import kotlin.io.path.readText
 
-private val logger = KotlinLogging.logger {  }
+val logger = ClusterLogger()
+
 class Cluster {
-    val clusterToml = Paths.get("cluster.toml")
+    val clusterToml: Path = Paths.get("cluster.toml")
+
+    lateinit var terminal: JLineTerminal;
 
     fun load() {
+        logger.info("»")
+        logger.info("<gray>easycloud<white>@<blue>${System.getProperty("version")}<white>:<gray> ")
+
         // Writing default configuration to cluster.toml if it does not exist
         clusterToml.takeIf { !it.exists() }.apply {
             TomlFileWriter().encodeToFile(serializer(), ClusterToml(), clusterToml.toString())
         }
 
-       // logger.info { Toml().encodeToString(serializer(), clusterToml.toString()) }
+        this.terminal = JLineTerminal()
 
-        val config = TomlFileReader.decodeFromString(ClusterToml.serializer(), clusterToml.readText())
-
-        logger.info { config.network.host }
-
-        logger.info { "Loading cluster..." }
+        logger.info("Loading cluster...")
     }
 
     fun run() {
-        logger.info { "Running cluster..." }
+        this.terminal.start()
+
+        logger.info("Running cluster...")
     }
 }
---
+++ cluster/src/main/kotlin/dev/easycloud/terminal/logger/ClusterLogger.kt
@@ -0,0 +1,46 @@
+package dev.easycloud.terminal.logger
+
+class ClusterLogger {
+    fun info(message: String) {
+        println(this.format("info", message))
+    }
+
+    fun warn(message: String) {
+        println(this.format("warn", message))
+    }
+
+    fun error(message: String) {
+        println(this.format("error", message))
+    }
+
+    fun debug(message: String) {
+        println(this.format("debug", message))
+    }
+
+    fun trace(message: String) {
+        println(this.format("trace", message))
+    }
+
+    fun colorText(message: String, vararg args: Any): String {
+        return Color.entries.fold(message) { it, color ->
+            it.replace("<" + color.name.lowercase() + ">", color.ascii)
+        }.format(*args)
+    }
+
+    fun format(type: String, message: String, vararg args: Any): String {
+        return "[${type}] ${colorText(message, *args)}"
+    }
+}
+
+enum class Color(val ascii: String) {
+    RESET("\u001B[0m"),
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[94m"),
+    PURPLE("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    GRAY("\u001B[37m"),
+    WHITE("\u001B[97m");
+}
